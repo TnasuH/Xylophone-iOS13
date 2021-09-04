b6403ceeb9950fa24b6eb654f7afbc6d9abb224c
Tarık Nasuhoğlu <tnasuh@gmail.com>
2021-09-04 21:18:37 +0300

    dev completed with asyncAfter

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -12,16 +12,23 @@ import AVFoundation
 class ViewController: UIViewController {
     
     var player: AVAudioPlayer!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
     @IBAction func keyPressed(_ sender: UIButton) {
-        playSound(name: sender.currentTitle!)
+        playSound(sender.currentTitle!)
+        sender.alpha = 0.5
+        
+        //Code should execute after 0.2 second delay.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            //Bring's sender's opacity back up to fully opaque.
+            sender.alpha = 1.0
+        }
     }
     
-    func playSound(name: String) {
+    func playSound(_ name: String) {
         let url = Bundle.main.url(forResource: name, withExtension: "wav")
         player = try! AVAudioPlayer(contentsOf: url!)
         player.play()
